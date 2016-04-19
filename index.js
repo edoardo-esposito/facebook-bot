b6f79f2ced5a12444afc54b43a3e677b9395ca5b
Edoardo Esposito <edoardo.esposito@gmail.com>
2016-04-19 12:46:18 +0200

    Template message

--- index.js
+++ index.js
@@ -8,20 +8,13 @@ var fb_page_token = "CAADOMHoG70YBAJzbsoIRZAExczO6degnyMSCfriL1s5M234LynybVcal4t
 var fb_verify_token = "jsfkfiwnfiwfwf283712";
 
 app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
 app.use(bodyParser.json())
 
-// Index route
 app.get('/', function (req, res) {
-    console.log('Hello world, I am a chat bot')
-    res.send('Hello world, I am a chat bot')
+    res.send('Chat bot')
 })
 
-// for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === fb_verify_token) {
         res.send(req.query['hub.challenge'])
@@ -31,7 +24,6 @@ app.get('/webhook/', function (req, res) {
 
 app.post('/webhook/', function (req, res) {
 	messaging_events = req.body.entry[0].messaging
-	console.log(messaging_events);
 	for (i = 0; i < messaging_events.length; i++) {
 	    event = req.body.entry[0].messaging[i]
 	    sender = event.sender.id;
@@ -39,7 +31,13 @@ app.post('/webhook/', function (req, res) {
 	    if (event.message && event.message.text) {
 	    	text = event.message.text
 		console.log("Text: " + text);
-	        sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	
+	            if (text === 'prova') {
+	                sendGenericMessage(sender)
+	                continue
+	            }
+
+	        sendTextMessage(sender, "Hai scritto: " + text.substring(0, 200))
 	    }
     }
     res.sendStatus(200)
@@ -51,6 +49,9 @@ function sendTextMessage(sender, text) {
 	messageData = {
 		text:text
 	}
+
+	console.log("Sender: " + sender);
+
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {access_token:token},
@@ -60,8 +61,6 @@ function sendTextMessage(sender, text) {
 			message: messageData,
 		}
 	}, function(error, response, body) {
-		console.log(body);
-		console.log(response);
 
 		if (error) {
 			console.log('Error sending messages: ', error)
@@ -69,6 +68,55 @@ function sendTextMessage(sender, text) {
 			console.log('Error: ', response.body.error)
 		}
 	})
+}
+
+function sendGenericMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
 }
 
 // Spin up the server
