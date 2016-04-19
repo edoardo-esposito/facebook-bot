c89897ea6ffeae439c35a911845716be1c3943d4
Edoardo Esposito <edoardo.esposito@gmail.com>
2016-04-19 12:28:45 +0200

    First messaging code

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 
+var fb_page_token = "CAADOMHoG70YBAIulwTUWmB5QUv430Kfg0CUIEAD8TZBSStRAUpoEPLuDz58rrUbR87bSMIEDlOBP1VyBZBEazTj9ZBnXaVxaWMpkeT8DddoCZBCZBhAO7QKcODBjihZA1juhaveaDa7rgREzLyrZCddZBucSZBwjfTbcwNRl9wsa4dcWNZCrZAL1zBXNk7d8UE1vGwZD";
+var fb_verify_token = "jsfkfiwnfiwfwf283712";
+
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -18,11 +21,48 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'jsfkfiwnfiwfwf283712') {
+    if (req.query['hub.verify_token'] === fb_verify_token) {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 })
+
+app.post('/webhook/', function (req, res) {
+	messaging_events = req.body.entry[0].messaging
+	for (i = 0; i < messaging_events.length; i++) {
+	    event = req.body.entry[0].messaging[i]
+	    sender = event.sender.id;
+
+	    if (event.message && event.message.text) {
+	    	text = event.message.text
+	        sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
+var token = fb_page_token;
+
+function sendTextMessage(sender, text) {
+	messageData = {
+		text:text
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
